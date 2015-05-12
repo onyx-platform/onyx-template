7b5692ad669183dcf07d8c35bf5eaa28e2dc9652
Michael Drogalis <mjd3089@rit.edu>
2015-05-11 19:35:20 -0700

    GitHub URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject onyx/lein-template "0.6.0-SNAPSHOT"
   :description "Onyx 0.6.0 Leiningen application template"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
