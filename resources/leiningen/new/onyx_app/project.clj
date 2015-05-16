c849d5f188fb08748553e2217d7e4a5c37e81ca1
Christopher Small <metasoarous@gmail.com>
2015-05-16 10:12:09 -0700

    Added slf4j impl to deps; solves #2

--- resources/leiningen/new/onyx_app/project.clj
+++ resources/leiningen/new/onyx_app/project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.mdrogalis/onyx "0.6.0-SNAPSHOT"]
+                 [org.slf4j/slf4j-simple "1.6.1"]
                  [com.stuartsierra/component "0.2.3"]
                  [fipp "0.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
