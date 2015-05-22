56d3fe4e363eec031b82d4400ca476c6a832117c
Michael Drogalis <mjd3089@rit.edu>
2015-05-21 17:00:45 -0700

    0.6.0-beta1

--- resources/leiningen/new/onyx_app/project.clj
+++ resources/leiningen/new/onyx_app/project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.mdrogalis/onyx "0.6.0-SNAPSHOT"]
+                 [com.mdrogalis/onyx "0.6.0-beta1"]
                  [com.stuartsierra/component "0.2.3"]
                  [fipp "0.6.1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
