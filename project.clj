23185ef30feff87a98226e6bb63dd5c0a3f6a55a
Michael Drogalis <mjd3089@rit.edu>
2015-07-07 08:04:35 -0700

    Bump version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.6.0.3"
+(defproject onyx-app/lein-template "0.6.0.4"
   :description "Onyx 0.6.0 Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
