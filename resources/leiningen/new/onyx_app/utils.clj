a4425947f74eeacb62a341f9e4a3e45db1f656b0
Lucas Bradstreet <lucas.bradstreet@onyxplatform.org>
2015-08-11 21:50:10 +0100

    Add an update-task fn that allows a single catalog entry to be updated

--- resources/leiningen/new/onyx_app/utils.clj
+++ resources/leiningen/new/onyx_app/utils.clj
@@ -18,12 +18,25 @@
     result
     (assert false)))
 
-(defn find-task [catalog task-name]
+(defn find-task 
+  "Finds the catalog entry where the :onyx/name key equals task-name"
+  [catalog task-name]
   (let [matches (filter #(= task-name (:onyx/name %)) catalog)]
     (when-not (seq matches)
       (throw (ex-info (format "Couldn't find task %s in catalog" task-name)
                       {:catalog catalog :task-name task-name})))
     (first matches)))
+
+(defn update-task 
+  "Finds the catalog entry with :onyx/name task-name
+  and applies f to it, returning the full catalog with the 
+  transformed catalog entry"
+  [catalog task-name f]
+  (mapv (fn [entry]
+          (if (= task-name (:onyx/name entry))
+            (f entry)
+            entry))
+        catalog))
 
 (defn n-peers
   "Takes a workflow and catalog, returns the minimum number of peers
