6c46dd128225c0033c26f0264ee32f83448e084d
Michael Drogalis <mjd3089@rit.edu>
2015-11-04 13:26:37 -0800

    0.8.0.0-alpha1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.7.5.3"
+(defproject onyx-app/lein-template "0.8.0.0-alpha1"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
