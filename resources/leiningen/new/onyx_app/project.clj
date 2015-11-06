8129766966858a538539287c67066bc0fa1c0ce1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-06 20:46:10 +0800

    Drop core.async/component dependency These will force older versions than Onyx use internally, which is probably a bad idea

--- resources/leiningen/new/onyx_app/project.clj
+++ resources/leiningen/new/onyx_app/project.clj
@@ -4,8 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [org.onyxplatform/onyx "0.8.0-alpha1"]
-                 [com.stuartsierra/component "0.2.3"]]
+                 [org.onyxplatform/onyx "0.8.0-alpha1"]]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
                    :source-paths ["env/dev" "src"]}})
