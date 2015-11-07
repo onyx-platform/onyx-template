55a6d6638f14c1d155e152ec89c30f3f6c1a0257
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-07 14:16:05 +0800

    Print that we're blocking forever in prod peers.

--- resources/leiningen/new/onyx_app/launch_prod_peers_bare.clj
+++ resources/leiningen/new/onyx_app/launch_prod_peers_bare.clj
@@ -9,16 +9,17 @@
 
 (defn -main [onyx-id n & args]
   (let [n-peers (Integer/parseInt n)
-        peer-config (assoc (-> "prod-peer-config.edn"
-                               resource slurp read-string) :onyx/id onyx-id)
-        peer-group (onyx.api/start-peer-group peer-config)
-        peers (onyx.api/start-peers n-peers peer-group)]
+	peer-config (assoc (-> "prod-peer-config.edn"
+			       resource slurp read-string) :onyx/id onyx-id)
+	peer-group (onyx.api/start-peer-group peer-config)
+	peers (onyx.api/start-peers n-peers peer-group)]
     (.addShutdownHook (Runtime/getRuntime)
-                      (Thread.
-                       (fn []
-                         (doseq [v-peer peers]
-                           (onyx.api/shutdown-peer v-peer))
-                         (onyx.api/shutdown-peer-group peer-group)
-                         (shutdown-agents))))
+		      (Thread.
+			(fn []
+			  (doseq [v-peer peers]
+			    (onyx.api/shutdown-peer v-peer))
+			  (onyx.api/shutdown-peer-group peer-group)
+			  (shutdown-agents))))
+    (println "Started peers. Blocking forever.")
     ;; Block forever.
     (<!! (chan))))
