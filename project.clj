8672118b85727644759f468a326e6b501c9f59d0
Lucas Bradstreet <lucasbradstreet@gmail.com>
2015-11-27 16:17:27 +0800

    Add template to release process

--- project.clj
+++ project.clj
@@ -3,5 +3,16 @@
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
   :plugins [[lein-set-version "0.4.1"]]
+  :profiles {:dev {:plugins [[lein-set-version "0.4.1"]
+                             [lein-update-dependency "0.1.2"]
+                             [lein-pprint "1.1.1"]]}}
   :eval-in-leiningen true)
