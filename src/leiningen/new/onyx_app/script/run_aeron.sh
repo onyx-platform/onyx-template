bf3b30ef0a17dc9badcc71b838e3ecec5b53e16b
Gardner Vickers <gvickers@clarityssi.com>
2015-12-03 15:56:58 -0500

    Started using my_init as provided by phusion
    base image. This gets us around the PID 1
    problem with docker. #22

---
+++ src/leiningen/new/onyx_app/script/run_aeron.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Setting shared memory for Aeron"
+mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
+APP_NAME=$(echo "{{app-name}}" | sed s/"-"/"_"/g)
+
+exec /sbin/setuser onyx java -cp /srv/{{app-name}}.jar "$APP_NAME.launcher.aeron_media_driver" >>/var/log/aeron.log 2>&1
