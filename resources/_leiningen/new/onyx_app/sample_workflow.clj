446ef5cdc6437466ba29c1fb1135f65b4677488b
Gardner Vickers <gvickers@clarityssi.com>
2015-12-02 21:38:09 -0500

    Deleted old file locations

--- resources/_leiningen/new/onyx_app/sample_workflow.clj
+++
@@ -1,9 +0,0 @@
-(ns {{app-name}}.workflows.sample-workflow)
-
-;;; The workflow of an Onyx job describes the graph of all possible
-;;; tasks that data can flow between.
-
-(defn build-workflow [ctx]
-  [[:read-lines :format-line]
-   [:format-line :upper-case]
-   [:upper-case :write-lines]])
