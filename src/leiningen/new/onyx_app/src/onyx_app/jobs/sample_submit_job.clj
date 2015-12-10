050ddee3565f48f8843f723ed7fb3d0ffa690389
Gardner Vickers <gvickers@clarityssi.com>
2015-12-10 11:03:10 -0500

    Fixed launching a job to docker compose

--- src/leiningen/new/onyx_app/src/onyx_app/jobs/sample_submit_job.clj
+++ src/leiningen/new/onyx_app/src/onyx_app/jobs/sample_submit_job.clj
@@ -1,17 +1,17 @@
 (ns {{app-name}}.jobs.sample-submit-job
-  (:require [clojure.java.io :refer [resource]]
-            [com.stuartsierra.component :as component]
-            [onyx.test-helper :refer [load-config]]
-            [{{app-name}}.plugins.http-reader :refer [add-http-reader]]
-            [{{app-name}}.catalogs.sample-catalog :refer [build-catalog]]
-            [{{app-name}}.lifecycles.sample-lifecycle :refer [build-lifecycles
-                                                              add-core-async
-                                                              add-logging]]
-            [{{app-name}}.workflows.sample-workflow :refer [build-workflow]]
-            [{{app-name}}.flow-conditions.sample-flow-conditions :refer [build-flow-conditions]]
-            [{{app-name}}.sample-input :as dev-inputs]
-            [{{app-name}}.functions.sample-functions]
-            [onyx.api]))
+    (:require [clojure.java.io :refer [resource]]
+              [com.stuartsierra.component :as component]
+              [onyx.test-helper :refer [load-config]]
+              [{{app-name}}.plugins.http-reader :refer [add-http-reader]]
+              [{{app-name}}.catalogs.sample-catalog :refer [build-catalog]]
+              [{{app-name}}.lifecycles.sample-lifecycle :refer [build-lifecycles
+                                                                add-core-async
+                                                                add-logging]]
+              [{{app-name}}.workflows.sample-workflow :refer [build-workflow]]
+              [{{app-name}}.flow-conditions.sample-flow-conditions :refer [build-flow-conditions]]
+              [{{app-name}}.sample-input :as dev-inputs]
+              [{{app-name}}.functions.sample-functions]
+              [onyx.api]))
 
 ;;;; Lets build a job
 (defn build-job [mode]
@@ -26,15 +26,16 @@
                       :flow-conditions (build-flow-conditions {:mode mode})
                       :task-scheduler :onyx.task-scheduler/balanced}
         job          (cond-> base-job
-                             core-async?  (add-core-async)
-                             http-reader? (add-http-reader)
-                             logging      (add-logging logging))]
+                       core-async?  (add-core-async)
+                       http-reader? (add-http-reader)
+                       logging      (add-logging logging))]
     job))
 
 (defn -main [onyx-id & args]
-  (let [config (assoc-in (load-config "config.edn")
-                              [:peer-config :onyx/id] onyx-id)
-        peer-config (get config :peer-config)]
+  (let [config (load-config "config.edn")
+        peer-config (-> (get config :peer-config)
+                        (assoc :onyx/id onyx-id)
+                        (assoc :zookeeper/address "192.168.99.100"))]
+    (clojure.pprint/pprint peer-config)
     (let [job (build-job :prod)]
-      (onyx.api/submit-job peer-config job)
-      (shutdown-agents))))
+      (onyx.api/submit-job peer-config job))))
