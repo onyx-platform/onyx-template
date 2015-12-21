060dc08d04f5bb1e3ac23584e3014b3c1328f06b
Gardner Vickers <gvickers@clarityssi.com>
2015-12-21 18:55:02 -0500

    Only build the peer from build.sh

--- src/leiningen/new/onyx_app/script/build.sh
+++ src/leiningen/new/onyx_app/script/build.sh
@@ -2,5 +2,4 @@
 set -e
 lein clean
 lein uberjar
-{{#docker?}}docker build -f script/kafka-meetup-streamer/Dockerfile -t {{app-name}}/kafka-meetup-streamer:0.1.0 . {{/docker?}}
 {{#docker?}}docker build -t {{app-name}}:0.1.0 . {{/docker?}}
