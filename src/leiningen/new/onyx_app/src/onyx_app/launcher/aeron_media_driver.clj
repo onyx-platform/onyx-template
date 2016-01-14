40a5c14cec2512cb3fad955286f6063d84911eb9
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-01-14 20:47:07 +0800

    Do not allow conflict with aeron property and command line arg.

--- src/leiningen/new/onyx_app/src/onyx_app/launcher/aeron_media_driver.clj
+++ src/leiningen/new/onyx_app/src/onyx_app/launcher/aeron_media_driver.clj
@@ -5,16 +5,20 @@
            [uk.co.real_logic.aeron.driver MediaDriver MediaDriver$Context ThreadingMode]))
 
 (defn -main [& [threading-mode]]
+  (when (and (System/getProperty "aeron.threading.mode")
+             threading-mode)
+    (throw (Exception. "Cannot set both aeron.threading.mode property and threading-mode command-line arg")))
+
   (let [threading-mode (cond (or (nil? threading-mode)
-                                 (= threading-mode "dedicated"))
-                                 ThreadingMode/DEDICATED
-                                 (= threading-mode "shared")
-                                 ThreadingMode/SHARED
-                                 (= threading-mode "shared-network")
-                                 ThreadingMode/SHARED_NETWORK)
-        _ (println "Starting media driver with threading mode: " threading-mode)
-        ctx (-> (MediaDriver$Context.)
-                (.threadingMode threading-mode))
+                                 (= threading-mode "shared"))
+                             ThreadingMode/SHARED
+                             (= threading-mode "dedicated")
+                             ThreadingMode/DEDICATED
+                             (= threading-mode "shared-network")
+                             ThreadingMode/SHARED_NETWORK)
+        _ (println "Starting media driver with threading mode:" threading-mode)
+        ctx (cond-> (MediaDriver$Context.)
+              threading-mode (.threadingMode threading-mode))
         media-driver (MediaDriver/launch ctx)]
     (println "Launched the Media Driver. Blocking forever...")
     (<!! (chan))))
