c031dad36c193a04846da8bed0418881fb7bf160
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-02 16:57:09 +0800

    Fix println

--- src/leiningen/new/onyx_app/src/onyx_app/launcher/launch_prod_peers.clj
+++ src/leiningen/new/onyx_app/src/onyx_app/launcher/launch_prod_peers.clj
@@ -21,7 +21,7 @@
         peer-group (onyx.api/start-peer-group peer-config)
         env (onyx.api/start-env (:env-config config))
         peers (onyx.api/start-peers n-peers peer-group)]
-    (println "Attempting to connect to to Zookeeper: " (:zookeeper/address peer-config))
+    (println "Attempting to connect to Zookeeper @" (:zookeeper/address peer-config))
     (.addShutdownHook (Runtime/getRuntime)
                       (Thread.
                         (fn []
