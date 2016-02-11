8b009543bea6025245d2ec117dfd45c878058823
Gardner Vickers <gardner@vickers.me>
2016-02-11 10:36:05 -0500

    Added option arity for metrics behavior

--- src/leiningen/new/onyx_app/src/onyx_app/behaviors/metrics.clj
+++ src/leiningen/new/onyx_app/src/onyx_app/behaviors/metrics.clj
@@ -2,8 +2,9 @@
 
 (defn add-metrics
   "Add's throughput and latency metrics to a task"
-  [job task opts]
-  (-> job
-      (update-in [:lifecycles] conj (merge {:lifecycle/task task ; Or :all for all tasks in the workflow
-                                            :lifecycle/calls :onyx.lifecycle.metrics.metrics/calls}
-                                           opts))))
+  ([job task] (add-metrics job task {}))
+  ([job task opts]
+   (-> job
+       (update-in [:lifecycles] conj (merge {:lifecycle/task task ; Or :all for all tasks in the workflow
+                                             :lifecycle/calls :onyx.lifecycle.metrics.metrics/calls}
+                                            opts)))))
