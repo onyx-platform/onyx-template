8b39c6ad394799536c5a2da5da7026aafe2b1eba
Michael Drogalis <mjd3089@rit.edu>
2016-03-07 11:12:58 -0800

    Release version 0.8.11.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.8.11.3-SNAPSHOT"
+(defproject onyx-app/lein-template "0.8.11.3"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
