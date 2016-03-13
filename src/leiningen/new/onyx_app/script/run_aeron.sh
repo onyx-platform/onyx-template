30f1848ae2f50386cc53088a6eb4964e839e7342
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-03-13 23:15:37 +0800

    Fix run scripts due to unbound variables

--- src/leiningen/new/onyx_app/script/run_aeron.sh
+++ src/leiningen/new/onyx_app/script/run_aeron.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o xtrace
 
 echo "Setting shared memory for Aeron"
 mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
 APP_NAME=$(echo "{{app-name}}" | sed s/"-"/"_"/g)
 
-exec java $MEDIA_DRIVER_JAVA_OPTS -cp /srv/{{app-name}}.jar "$APP_NAME.launcher.aeron_media_driver" >>/var/log/aeron.log 2>&1
+exec java ${MEDIA_DRIVER_JAVA_OPTS:-} -cp /srv/{{app-name}}.jar "$APP_NAME.launcher.aeron_media_driver" >>/var/log/aeron.log 2>&1
--- src/leiningen/new/onyx_app/script/run_peers.sh
+++ src/leiningen/new/onyx_app/script/run_peers.sh
@@ -5,4 +5,4 @@ set -o xtrace
 
 export BIND_ADDR="${BIND_ADDR:-$(hostname --ip-address)}"
 export APP_NAME=$(echo "{{app-name}}" | sed s/"-"/"_"/g)
-exec java $PEER_JAVA_OPTS -cp /srv/{{app-name}}.jar "$APP_NAME.launcher.launch_prod_peers" $NPEERS {{^docker?}}>>/var/log/onyx.log 2>&1{{/docker?}}
+exec java ${PEER_JAVA_OPTS:-} -cp /srv/{{app-name}}.jar "$APP_NAME.launcher.launch_prod_peers" $NPEERS {{^docker?}}>>/var/log/onyx.log 2>&1{{/docker?}}
