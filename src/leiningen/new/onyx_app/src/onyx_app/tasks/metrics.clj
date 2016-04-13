8e90eeb46e2ca14b79dbed13424cfff94aef481c
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-04-13 17:46:55 +0800

    Log metrics to timbre properly

--- src/leiningen/new/onyx_app/src/onyx_app/tasks/metrics.clj
+++ src/leiningen/new/onyx_app/src/onyx_app/tasks/metrics.clj
@@ -6,8 +6,8 @@
   ([job task opts]
    (-> job
        (update-in [:lifecycles] conj (merge {:lifecycle/task task ; Or :all for all tasks in the workflow
-                                             :lifecycle/calls :onyx.lifecycle.metrics.metrics/calls
                                              :metrics/buffer-capacity 10000
                                              :metrics/sender-fn :onyx.lifecycle.metrics.timbre/timbre-sender
+                                             :lifecycle/doc "Instruments task metrics to timbre"
                                              :lifecycle/calls :onyx.lifecycle.metrics.metrics/calls}
                                             opts)))))
