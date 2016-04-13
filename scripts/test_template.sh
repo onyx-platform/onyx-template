5d7cd1a9655cb968704a56c189f5833991e6e8b1
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-04-13 18:11:47 +0800

    Cleanup lein-template in m2

--- scripts/test_template.sh
+++ scripts/test_template.sh
@@ -6,3 +6,4 @@ lein new onyx-app test-app-docker-metrics -- +docker +metrics
 cd test-app-docker-metrics && lein test && cd ..
 rm -rf test-app-docker-metrics
 git checkout project.clj
+rm -rf ~/.m2/repository/onyx-app/lein-template/99.99.99/
