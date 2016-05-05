c7cb1f9714cae3f9f6db4aa87466a21eabb4720f
OnyxBot <michael.drogalis@onyxplatform.org>
2016-05-05 20:35:45 +0000

    Release version 0.9.6.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.9.4.1-SNAPSHOT"
+(defproject onyx-app/lein-template "0.9.6.0"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "Eclipse Public License"
