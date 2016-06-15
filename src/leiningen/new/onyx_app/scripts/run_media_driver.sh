cf460a038aa0e62605954edebad0bb87c2d1d015
Gardner Vickers <gardner@vickers.me>
2016-06-15 00:58:28 -0400

    added bash and better defaults

--- src/leiningen/new/onyx_app/scripts/run_media_driver.sh
+++ src/leiningen/new/onyx_app/scripts/run_media_driver.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-/opt/jdk/bin/java -cp /opt/peer.jar "lib_onyx.media_driver"
+CGROUPS_MEM=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+MEMINFO_MEM=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo)*1024))
+MEM=$(($MEMINFO_MEM>$CGROUPS_MEM?$CGROUPS_MEM:$MEMINFO_MEM))
+JVM_MEDIA_DRIVER_HEAP_RATIO=${JVM_MEDIA_DRIVER_HEAP_RATIO:-0.2}
+XMX=$(awk '{printf("%d",$1*$2/1024^2)}' <<< " ${MEM} ${JVM_MEDIA_DRIVER_HEAP_RATIO} ")
+# Use the container memory limit to set max heap size so that the GC
+# knows to collect before it's hard-stopped by the container environment,
+# causing OOM exception.
+/opt/jdk/bin/java $MEDIA_DRIVER_JAVA_OPTS \
+                  "-Xmx${XMX}m" \
+                  -cp /opt/peer.jar \
+                  lib_onyx.media_driver
--- src/leiningen/new/onyx_app/scripts/run_peer.sh
+++ src/leiningen/new/onyx_app/scripts/run_peer.sh
@@ -1,3 +1,18 @@
-#!/bin/sh
+#!/bin/bash
+#export MALLOC_ARENA_MAX=4 # Stop the JVM from being allowed to use up all of
+# Docker's virtual memory. Use if it's a problem
+# see https://siddhesh.in/posts/malloc-per-thread-arenas-in-glibc.html
 
-/opt/jdk/bin/java -cp /opt/peer.jar {{app-name-underscore}}.core start-peers "$NPEERS" -p :docker
+CGROUPS_MEM=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes)
+MEMINFO_MEM=$(($(awk '/MemTotal/ {print $2}' /proc/meminfo)*1024))
+MEM=$(($MEMINFO_MEM>$CGROUPS_MEM?$CGROUPS_MEM:$MEMINFO_MEM))
+JVM_PEER_HEAP_RATIO=${JVM_PEER_HEAP_RATIO:-0.6}
+XMX=$(awk '{printf("%d",$1*$2/1024^2)}' <<< " ${MEM} ${JVM_PEER_HEAP_RATIO} ")
+# Use the container memory limit to set max heap size so that the GC
+# knows to collect before it's hard-stopped by the container environment,
+# causing OOM exception.
+
+/opt/jdk/bin/java $PEER_JAVA_OPTS \
+                  "-Xmx${XMX}m" \
+                  -cp /opt/peer.jar \
+                  {{app-name-underscore}}.core start-peers "$NPEERS" -p :docker
