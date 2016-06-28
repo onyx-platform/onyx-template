7d8769c923e023a2ed7c1b34d245c52693230743
Gardner Vickers <gardner@vickers.me>
2016-06-28 19:01:19 -0400

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.9.0.10"
+(defproject onyx-app/lein-template "0.9.0.11-SNAPSHOT"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "MIT License"
