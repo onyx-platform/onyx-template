6cec649d3169f65994588290410bc4f28366eca8
OnyxBot <michael.drogalis@onyxplatform.org>
2016-06-30 22:37:24 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.9.7.0-alpha7"
+(defproject onyx-app/lein-template "0.9.7.0-SNAPSHOT"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "MIT License"
