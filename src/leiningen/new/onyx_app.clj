857a8d74dbab960e23620f3bdf902ccb3cc8e966
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-09-14 22:17:07 +0800

    Use new onyx version / lib-onyx

--- src/leiningen/new/onyx_app.clj
+++ src/leiningen/new/onyx_app.clj
@@ -37,8 +37,8 @@
         data {:name name
               ;; The formatting here matters
               :onyx-version "0.9"
-              :onyx-version-post ".9"
-              :lib-onyx-minor "0.1"
+              :onyx-version-post ".10"
+              :lib-onyx-minor "10.0"
               :app-name name
               :app-name-underscore (clojure.string/replace name #"-" "_")
               :sanitized path
