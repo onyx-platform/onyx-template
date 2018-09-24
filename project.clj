4aad8bc4a61c9646b5e1cbb9e612c3eab91023b6
OnyxBot <michael.drogalis@onyxplatform.org>
2018-09-24 04:53:49 +0000

    Release version 0.13.5.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-app/lein-template "0.13.4.1-SNAPSHOT"
+(defproject onyx-app/lein-template "0.13.5.0"
   :description "Onyx Leiningen application template"
   :url "https://github.com/onyx-platform/onyx-template"
   :license {:name "MIT License"
